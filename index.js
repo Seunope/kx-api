90026cca30d2b0ce8678b458f6c4ac683a3a949f
Seunope <seunope2004@yahoo.co.uk>
2018-07-18 18:04:12 +0100

    fix server port

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ const server = new ApolloServer({ typeDefs, resolvers });
 
 // This `listen` method launches a web-server.  Existing apps
 // can utilize middleware options, which we'll discuss later.
-server.listen().then(({ url }) => {
+server.listen({port:process.env.PORT || 3000}).then(({ url }) => {
   console.log(`🚀  Server ready at ${url}`);
 });
